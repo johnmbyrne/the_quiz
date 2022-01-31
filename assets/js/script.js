086b88af81ba74db249d9b2606c8a2b7dfb37362
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-01-31 13:11:01 +0000

    Add placeholder questionbank in JS script file

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// General Knowledge Question bank
+
+const generalKnowledge = [
+    {
+        question: "Ask question?",
+        answerA: "A",
+        answerB: "B",
+        answerC: "C",
+        answerD: "D",
+        correctAnswer: "answerA"
+    },
+    
+]
