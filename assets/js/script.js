67994de7be9e95f91c600a2199fa333b534350e7
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-01-31 15:02:20 +0000

    Add 1st sample question, and placeholders for remaining questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // General Knowledge Question bank
+// Selection of 20 questions taken from: https://www.quiztriviagames.com/multiple-choice-trivia-questions/
 
 const generalKnowledge = [
+    {
+        question: "In which Italian city can you find the Colosseum?",
+        options: [
+            "Venice",
+            "Rome",
+            "Naples",
+            "Milan"
+        ]
+        correctAnswerIndex: 1
+    },
     {
         question: "Ask question?",
         options: [
@@ -11,6 +22,185 @@ const generalKnowledge = [
         ]
         correctAnswerIndex: 0
     },
-
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    },
+    {
+        question: "Ask question?",
+        options: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ]
+        correctAnswerIndex: 0
+    }
 ]
 
