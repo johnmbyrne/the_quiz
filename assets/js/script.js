3b78ec288d1156bf3ffc2fcd79a53296cf78772d
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-01-31 15:22:19 +0000

    Add first 10 questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,94 +13,94 @@ const generalKnowledge = [
         correctAnswerIndex: 1
     },
     {
-        question: "Ask question?",
+        question: "In the TV show New Girl, which actress plays Jessica Day?",
         options: [
-            "A",
-            "B",
-            "C",
-            "D"
+            "Zooey Deschanel",
+            "Kaley Cuoco",
+            "Jennifer Aniston",
+            "Alyson Hannigan"
         ]
         correctAnswerIndex: 0
     },
     {
-        question: "Ask question?",
+        question: "What is the largest canyon in the world?",
         options: [
-            "A",
-            "B",
-            "C",
-            "D"
+            "Verdon Gorge, France",
+            "King’s Canyon, Australia",
+            "Grand Canyon, USA",
+            "Fjaðrárgljúfur Canyon, Iceland"
+        ]
+        correctAnswerIndex: 2
+    },
+    {
+        question: "How long is the border between the United States and Canada?",
+        options: [
+            "3,525 miles",
+            "4,525 miles",
+            "5,525 miles",
+            "6,525 miles"
+        ]
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What is the largest active volcano in the world?",
+        options: [
+            "Mount Etna",
+            "Mount Vesuvius",
+            "Mouna Loa",
+            "Mount Batur"
+        ]
+        correctAnswerIndex: 2
+    },
+    {
+        question: "When did Lenin die?",
+        options: [
+            "21 January 1924",
+            "21 January 1934",
+            "21 January 1944",
+            "21 January 1954"
         ]
         correctAnswerIndex: 0
     },
     {
-        question: "Ask question?",
+        question: "In which US state did the ​​Scopes Monkey Trial happen?",
         options: [
-            "A",
-            "B",
-            "C",
-            "D"
+            "Maryland",
+            "South Dakota",
+            "Indiana",
+            "Tennessee"
+        ]
+        correctAnswerIndex: 3
+    },
+    {
+        question: "When was the book “To Kill a Mockingbird” by Harper Lee published?",
+        options: [
+            "1950",
+            "1960",
+            "1970",
+            "1980"
+        ]
+        correctAnswerIndex: 1
+    },
+    {
+        question: "In which museum can you find Leonardo Da Vinci’s Mona Lisa?",
+        options: [
+            "Le Louvre",
+            "Uffizi Museum",
+            "British Museum",
+            "Metropolitan Museum of Art"
         ]
         correctAnswerIndex: 0
     },
     {
-        question: "Ask question?",
+        question: "In which city can you find the Prado Museum?",
         options: [
-            "A",
-            "B",
-            "C",
-            "D"
+            "Buenos Aires",
+            "Barcelona",
+            "Santiago",
+            "Madrid"
         ]
-        correctAnswerIndex: 0
-    },
-    {
-        question: "Ask question?",
-        options: [
-            "A",
-            "B",
-            "C",
-            "D"
-        ]
-        correctAnswerIndex: 0
-    },
-    {
-        question: "Ask question?",
-        options: [
-            "A",
-            "B",
-            "C",
-            "D"
-        ]
-        correctAnswerIndex: 0
-    },
-    {
-        question: "Ask question?",
-        options: [
-            "A",
-            "B",
-            "C",
-            "D"
-        ]
-        correctAnswerIndex: 0
-    },
-    {
-        question: "Ask question?",
-        options: [
-            "A",
-            "B",
-            "C",
-            "D"
-        ]
-        correctAnswerIndex: 0
-    },
-    {
-        question: "Ask question?",
-        options: [
-            "A",
-            "B",
-            "C",
-            "D"
-        ]
-        correctAnswerIndex: 0
+        correctAnswerIndex: 3
     },
     {
         question: "Ask question?",
