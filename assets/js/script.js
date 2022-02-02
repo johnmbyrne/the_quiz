38f4764f6897407716c09b8c9539257bb07a3267
johnmbyrne <john.byrne@corvusconsulting.eu>
2022-02-02 16:47:16 +0000

    Change question and answer formats to move away from using indices as quiz wasn't picking them up correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,203 +4,163 @@
 const generalKnowledge = [
     {
         question: "In which Italian city can you find the Colosseum?",
-        options: [
-            "Venice",
-            "Rome",
-            "Naples",
-            "Milan"
-        ],
-        correctAnswerIndex: 1
+        answerA: "Venice",
+        answerB: "Rome",
+        answerC: "Naples",
+        answerD:  "Milan",
+        correctAnswer: "answerB"
     },
     {
         question: "In the TV show New Girl, which actress plays Jessica Day?",
-        options: [
-            "Zooey Deschanel",
-            "Kaley Cuoco",
-            "Jennifer Aniston",
-            "Alyson Hannigan"
-        ],
-        correctAnswerIndex: 0
+        answerA: "Zooey Deschanel",
+        answerB: "Kaley Cuoco",
+        answerC: "Jennifer Aniston",
+        answerD: "Alyson Hannigan",
+        correctAnswer: "answerA"
     },
     {
         question: "What is the largest canyon in the world?",
-        options: [
-            "Verdon Gorge, France",
-            "King’s Canyon, Australia",
-            "Grand Canyon, USA",
-            "Fjaðrárgljúfur Canyon, Iceland"
-        ],
-        correctAnswerIndex: 2
+        answerA:  "Verdon Gorge, France",
+        answerB:  "King’s Canyon, Australia",
+        answerC:  "Grand Canyon, USA",
+        answerD:  "Fjaðrárgljúfur Canyon, Iceland",
+        correctAnswer: "answerC"
     },
     {
         question: "How long is the border between the United States and Canada?",
-        options: [
-            "3,525 miles",
-            "4,525 miles",
-            "5,525 miles",
-            "6,525 miles"
-        ],
-        correctAnswerIndex: 2
+        answerA: "3,525 miles",
+        answerB: "4,525 miles",
+        answerC: "5,525 miles",
+        answerD: "6,525 miles",
+        correctAnswer: "answerC"
     },
     {
         question: "What is the largest active volcano in the world?",
-        options: [
-            "Mount Etna",
-            "Mount Vesuvius",
-            "Mouna Loa",
-            "Mount Batur"
-        ],
-        correctAnswerIndex: 2
+        answerA: "Mount Etna",
+        answerB: "Mount Vesuvius",
+        answerC: "Mouna Loa",
+        answerD: "Mount Batur",
+        correctAnswer: "answerC"
     },
     {
         question: "When did Lenin die?",
-        options: [
-            "21 January 1924",
-            "21 January 1934",
-            "21 January 1944",
-            "21 January 1954"
-        ],
-        correctAnswerIndex: 0
+        answerA:  "21 January 1924",
+        answerB:  "21 January 1934",
+        answerC:  "21 January 1944",
+        answerD:  "21 January 1954",
+        correctAnswer: "answerA"
     },
     {
-        question: "In which US state did the ​​Scopes Monkey Trial happen?",
-        options: [
-            "Maryland",
-            "South Dakota",
-            "Indiana",
-            "Tennessee"
-        ],
-        correctAnswerIndex: 3
+        question: "In which US state did the Scopes Monkey Trial happen?",
+        answerA: "Maryland",
+        answerB: "South Dakota",
+        answerC: "Indiana",
+        answerD: "Tennessee",
+        correctAnswer: "answerD"
     },
     {
         question: "When was the book “To Kill a Mockingbird” by Harper Lee published?",
-        options: [
-            "1950",
-            "1960",
-            "1970",
-            "1980"
-        ],
-        correctAnswerIndex: 1
+        answerA: "1950",
+        answerB: "1960",
+        answerC: "1970",
+        answerD: "1980",
+        correctAnswer: "answerB"
     },
     {
         question: "In which museum can you find Leonardo Da Vinci’s Mona Lisa?",
-        options: [
-            "Le Louvre",
-            "Uffizi Museum",
-            "British Museum",
-            "Metropolitan Museum of Art"
-        ],
-        correctAnswerIndex: 0
+        answerA: "Le Louvre",
+        answerB: "Uffizi Museum",
+        answerC: "British Museum",
+        answerD: "Metropolitan Museum of Art",
+        correctAnswer: "answerA"
     },
     {
         question: "In which city can you find the Prado Museum?",
-        options: [
-            "Buenos Aires",
-            "Barcelona",
-            "Santiago",
-            "Madrid"
-        ],
-        correctAnswerIndex: 3
+        answerA: "Buenos Aires",
+        answerB: "Barcelona",
+        answerC: "Santiago",
+        answerD: "Madrid",
+        correctAnswer: "answerD"
     },
     {
         question: "When did Salt Lake City host the Winter Olympics?",
-        options: [
-            "1992",
-            "1998",
-            "2002",
-            "2008"
-        ],
-        correctAnswerIndex: 2
+        answerA: "1992",
+        answerB: "1998",
+        answerC: "2002",
+        answerD: "2008",
+        correctAnswer: "answerC"
     },
     {
         question: "In the Big Bang Theory, what is the name of Sheldon and Leonard’s neighbour?",
-        options: [
-            "Penny",
-            "Patty",
-            "Lily",
-            "Jessie"
-        ],
-        correctAnswerIndex: 0
+        answerA: "Penny",
+        answerB: "Patty",
+        answerC: "Lily",
+        answerD: "Jessie",
+        correctAnswer: "answerA"
     },
     {
         question: "When did Hitler invade Poland?",
-        options: [
-            "1st September 1939",
-            "11th November 1939",
-            "8th May 1940",
-            "1st December 1940"
-        ],
-        correctAnswerIndex: 0
+        answerA: "1st September 1939",
+        answerB: "11th November 1939",
+        answerC: "8th May 1940",
+        answerD: "1st December 1940",
+        correctAnswer: "answerA"
     },
     {
         question: "What is the largest continent in size?",
-        options: [
-            "Asia",
-            "Africa",
-            "Europe",
-            "North America"
-        ],
-        correctAnswerIndex: 0
+        answerA: "Asia",
+        answerB: "Africa",
+        answerC: "Europe",
+        answerD: "North America",
+        correctAnswer: "answerA"
     },
     {
         question: "Which French king was nicknamed the “Sun King”?",
-        options: [
-            "Louis XVI",
-            "Charlemagne",
-            "Francis I",
-            "Louis XIV"
-        ],
-        correctAnswerIndex: 3
+        answerA: "Louis XVI",
+        answerB: "Charlemagne",
+        answerC: "Francis I",
+        answerD: "Louis XIV",
+        correctAnswer: "answerD"
     },
     {
-        question: "Who was George W. Bush’ vice president?",
-        options: [
-            "Kamala Harris",
-            "Joe Biden",
-            "Dick Cheney",
-            "Al Gore"
-        ],
-        correctAnswerIndex: 2
+        question: "Who was George W. Bush’s vice president?",
+        answerA: "Kamala Harris",
+        answerB: "Joe Biden",
+        answerC: "Dick Cheney",
+        answerD: "Al Gore",
+        correctAnswer: "answerC"
     },
     {
         question: "Which famous inventor invented the telephone?",
-        options: [
-            "Thomas Edison",
-            "Benjamin Franklin",
-            "Alexander Graham Bell",
-            "Nikola Tesla"
-        ],
-        correctAnswerIndex: 2
+        answerA: "Thomas Edison",
+        answerB: "Benjamin Franklin",
+        answerC: "Alexander Graham Bell",
+        answerD: "Nikola Tesla",
+        correctAnswer: "answerC"
     },
     {
         question: "If you are born on the 1st of January, which star sign are you?",
-        options: [
-            "Scorpio",
-            "Capricorn",
-            "Libra",
-            "Aries"
-        ],
-        correctAnswerIndex: 1
+        answerA: "Scorpio",
+        answerB: "Capricorn",
+        answerC: "Libra",
+        answerD: "Aries",
+        correctAnswer: "answerB"
     },
     {
         question: "How many wives had Henry VIII?",
-        options: [
-            "1",
-            "3",
-            "4",
-            "6"
-        ],
-        correctAnswerIndex: 3
+        answerA: "1",
+        answerB: "3",
+        answerC: "4",
+        answerD: "6",
+        correctAnswer: "answerD"
     },
     {
         question: "In which city is the Juventus Football Club based?",
-        options: [
-            "Turin",
-            "Barcelona",
-            "Manchester",
-            "Marseille"
-        ],
-        correctAnswerIndex: 0
+        answerA: "Turin",
+        answerB: "Barcelona",
+        answerC: "Manchester",
+        answerD: "Marseille",
+        correctAnswer: "answerA"
     }
 ]
 
@@ -229,17 +189,17 @@ function nextQuestion(index) {
     document.getElementById("question_display").innerHTML = questionNumber
     document.getElementById("quiz_score").innerHTML = playerScore
     document.getElementById("question_display").innerHTML = currentQuestion.question;
-    document.getElementById("option1_label").innerHTML = currentQuestion.options[0];
-    document.getElementById("option2_label").innerHTML = currentQuestion.options[1];
-    document.getElementById("option3_label").innerHTML = currentQuestion.options[2];
-    document.getElementById("option4_label").innerHTML = currentQuestion.options[3];
+    document.getElementById("option1_label").innerHTML = currentQuestion.answerA;
+    document.getElementById("option2_label").innerHTML = currentQuestion.answerB;
+    document.getElementById("option3_label").innerHTML = currentQuestion.answerC;
+    document.getElementById("option4_label").innerHTML = currentQuestion.answerD;
 }
 
 // checking if the correct answer was selected
 
 function checkForAnswer() {
     const currentQuestion = randomQuestions[indexNumber]
-    const currentQuestionAnswer = currentQuestion.correctAnswerIndex
+    const currentQuestionAnswer = currentQuestion.correctAnswer
     const options = document.getElementsByName("option");
     let correctOption = null
 
